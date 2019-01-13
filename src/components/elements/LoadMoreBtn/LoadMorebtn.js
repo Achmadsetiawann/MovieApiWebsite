074c0cfa70a4f74f084399aeef3c0c6c45533c24
unknown <achmadsetiawannn@gmail.com>
2019-01-13 21:07:54 +0700

    Home Clear

---
+++ src/components/elements/LoadMoreBtn/LoadMorebtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadMoreBtn.css';
+
+const LoadMorebtn = (props) => {
+    return (
+        <div className="rmdb-loadmorebtn" onClick={props.onClick}>
+            <p>{props.text}</p>
+        </div>
+    )
+}
+
+export default LoadMorebtn;
